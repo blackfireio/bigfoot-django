77214a6592caab80aeb88a76eb91452816f9ae21
Sümer Cip <sumerc@gmail.com>
2021-03-23 08:00:46 -0400

    FIX Add cache for the user_activity_text per user

--- bigfoot/templatetags/bigfoot_extras.py
+++ bigfoot/templatetags/bigfoot_extras.py
@@ -1,10 +1,15 @@
 from django import template
+from django.core.cache import cache
 
 register = template.Library()
 
 @register.filter(name='user_activity_text')
 def user_activity_text(user):
-    comment_count = user.get_recent_comments_count()
+    cache_key = f'user_activity_text_{user.id}'
+    comment_count = cache.get(cache_key)
+    if comment_count is None:
+        comment_count = user.get_recent_comments_count()
+        cache.set(cache_key, comment_count, 3600)
 
     if (comment_count > 50):
         return 'bigfoot fanatic'
