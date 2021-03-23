a573793c35d5745375db5f42b7f9ea8c06b182e1
Sümer Cip <sumerc@gmail.com>
2021-03-23 08:00:46 -0400

    FIX Add cache for the user_activity_text per user

--- bigfoot_django/settings.py
+++ bigfoot_django/settings.py
@@ -92,6 +92,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
